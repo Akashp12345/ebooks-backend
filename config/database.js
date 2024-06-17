f1a196a5e8abae2f55fc32da3b42c9766a544ccf
Akashp12345 <akash.p6201@gmail.com>
2024-06-17 18:06:56 +0530

    changed database configuration

--- config/database.js
+++ config/database.js
@@ -1,18 +1,31 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config');
-
-const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
-  host: config.db.host,
-  dialect: config.db.dialect,
-});
-
+require('dotenv').config(); // Ensure this is loaded to read environment variables
+const config=require('./config')
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.username,
+  config.db.password,
+  {
+    host: config.db.host,
+    port:  3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
+    logging: false,
+  }
+);
 
 const startDatabase = async () => {
   try {
     await sequelize.sync();
-    console.log('Database synchronized');
+    console.log('Database connected and authenticated successfully');
   } catch (err) {
-    console.error('Unable to synchronize the database:', err);
+    console.error('Unable to connect to the database:', err);
   }
 };
-module.exports ={startDatabase,sequelize};
+   
+module.exports = { startDatabase, sequelize };
