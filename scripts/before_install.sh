47c3c9739622cd29392e5412665c1e8f6c8a3071
Akashp12345 <akash.p6201@gmail.com>
2024-06-18 13:57:08 +0530

    changed scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,5 +6,5 @@ pm2 stop server || true
 # Remove any temporary files or logs
 # For example:
 # rm -rf /path/to/your/application/tmp/*
-
+sudo rm -f /home/ubuntu/ebooks-backend/.gitignore || true
 # Additional cleanup steps as needed
